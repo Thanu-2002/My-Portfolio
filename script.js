219a92135933f03e02d87dff84977d9a9b9ec518
Thanu-2002 <mt.didu9988@gmail.com>
2024-12-18 20:39:34 +0530

    Add animation for text

---
+++ script.js
@@ -0,0 +1,16 @@
+// let word = document.querySelectorAll(".word");
+// word.forEach((word)=>{
+//     let letters = word.textContent.split("");
+//     word.textContent="";
+//     letters.forEach((letter)=>{
+//         let span = document.createElement("span");
+//         span.textContent = letter;
+//         span.className = "letter";
+//         word.append(span);
+//     });
+// });
+//
+// let currentWordIndex = 0;
+// let maxWordIndex = word.length -1;
+// words[currentWordIndex].style.opacity = "1";
+//
